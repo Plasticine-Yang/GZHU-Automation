27509ca2c1e6b4449888d5b6fe4148ebe64650ab
plasticine9750 <975036719@qq.com>
2022-11-11 20:23:43 +0800

    chore: 更新打卡表单项按钮

--- packages/health-clock-in/src/index.ts
+++ packages/health-clock-in/src/index.ts
@@ -24,40 +24,6 @@ function run() {
 }
 
 /**
- * @description 填写并提交表单
- */
-async function inputAndSubmitForm(page: Page) {
-  try {
-    await Promise.all([
-      // 当日是否外出
-      clickItem(page, '#V1_CTRL238'),
-
-      // 是否接触过半个月内有疫情重点地区旅居史的人员
-      clickItem(page, '#V1_CTRL46'),
-
-      // 健康码是否为绿码
-      clickItem(page, '#V1_CTRL262'),
-
-      // 半个月内是否到过国内疫情重点地区
-      clickItem(page, '#V1_CTRL37'),
-
-      // 本人承诺对上述填报内容真实性负责，如有不实，本人愿意承担一切责任
-      clickItem(page, '#V1_CTRL82'),
-    ])
-
-    // 提交
-    await clickItem(page, '#form_command_bar .command_button')
-
-    // 点击确认
-    await clickItem(page, '.dialog_button')
-
-    logger.log('打卡成功！', '恭喜您健康打卡成功')
-  } catch (err) {
-    logger.error('打卡表单提交出错', err)
-  }
-}
-
-/**
  * @description 广州大学健康打卡
  */
 async function healthClockIn(username: string, password: string) {
@@ -90,6 +56,48 @@ async function healthClockIn(username: string, password: string) {
   } finally {
     // 关闭浏览器
     await browser.close()
+  }
+}
+
+/**
+ * @description 填写并提交表单
+ * @param formItemList 需要点击的表单项 id 列表
+ */
+async function inputAndSubmitForm(page: Page) {
+  const formItemSelectorList = [
+    // 身体状况是否正常: 是
+    '#V1_CTRL243',
+
+    // 当日是否外出: 否
+    '#V1_CTRL238',
+
+    // 是否接触过半个月内有疫情重点地区旅居史的人员: 否
+    '#V1_CTRL46',
+
+    // 健康码是否为绿码: 是
+    '#V1_CTRL262',
+
+    // 半个月内是否到过国内疫情重点地区: 否
+    '#V1_CTRL37',
+
+    // 本人承诺对上述填报内容真实性负责，如有不实，本人愿意承担一切责任
+    '#V1_CTRL82',
+  ]
+
+  try {
+    await Promise.all(
+      formItemSelectorList.map(selector => clickItem(page, selector)),
+    )
+
+    // 提交
+    await clickItem(page, '#form_command_bar .command_button')
+
+    // 点击确认
+    await clickItem(page, '.dialog_button')
+
+    logger.log('打卡成功！', '恭喜您健康打卡成功')
+  } catch (err) {
+    logger.error('打卡表单提交出错', err)
   }
 }
 
